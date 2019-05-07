5301901d9be616b95e084cd81e9307db0e01c6d3
Vinh Tran <vincen7.tran@gmail.com>
2019-05-07 15:08:37 -0700

    First Commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const CSVGenerator = require('./public/CSVGenerator');
+
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(bodyParser.json());
+
+app.use(express.static('public'));
+
+app.post('/upload_json', (req, res) => {
+  const CSV = CSVGenerator(req.body);
+  res.end(CSV);
+});
+
+app.listen(PORT, (() => console.log(`Listening on Port ${PORT}`)));
