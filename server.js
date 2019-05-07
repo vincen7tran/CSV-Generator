fed33d61c623b726ddf21721d9bee932adfa7a5a
Vinh Tran <vincen7.tran@gmail.com>
2019-05-07 16:22:25 -0700

    HBS Templating Implemented

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const fs = require('fs');
+const path = require('path');
+const hbs = require('hbs');
 const bodyParser = require('body-parser');
 const CSVGenerator = require('./public/CSVGenerator');
 
 const app = express();
 const PORT = 3000;
+
+const viewsPath = path.join(__dirname, './templates/views');
+
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
 
 app.use(bodyParser.urlencoded({
   extended: true
@@ -16,7 +23,7 @@ app.use(express.static('public'));
 
 app.post('/upload_json', (req, res) => {
   const CSV = CSVGenerator(req.body);
-  res.end(CSV);
+  res.render('upload', { CSV });
 });
 
 app.listen(PORT, (() => console.log(`Listening on Port ${PORT}`)));
