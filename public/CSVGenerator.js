a97f7189492ffe12338f96ca959321ce946765c1
Vinh Tran <vincen7.tran@gmail.com>
2019-05-07 17:34:21 -0700

    Revelations about JSON.parse

--- public/CSVGenerator.js
+++ public/CSVGenerator.js
@@ -1,19 +1,20 @@
 function CSVGenerator({data}) {
-  const obj = convertData(data);
+  //const obj = convertData(data);
+  const obj = data[data.length - 1] === ';' ? JSON.parse(data.slice(0, data.length - 1)) : JSON.parse(data);
   const fields = [...extractFields(obj)];
   const extract = extractData(obj, fields)
   const csv = formatCSV(extract, fields);
   return csv;
 };
 
-function convertData(data) {
-  const removeSpace = data.split(' ');
-  const removeBreaks = removeSpace.join('').split('\r\n');
-  let last = removeBreaks[removeBreaks.length - 1];
-  // Remove Semicolon If Present
-  if (last[last.length - 1] === ';') removeBreaks[removeBreaks.length - 1] = removeBreaks[removeBreaks.length - 1].slice(0,1);
-  return JSON.parse(removeBreaks.join(''));
-}
+// function convertData(data) {
+//   const removeSpace = data.split(' ');
+//   const removeBreaks = removeSpace.join('').split('\r\n');
+//   let last = removeBreaks[removeBreaks.length - 1];
+//   // Remove Semicolon If Present
+//   if (last[last.length - 1] === ';') removeBreaks[removeBreaks.length - 1] = removeBreaks[removeBreaks.length - 1].slice(0,1);
+//   return JSON.parse(removeBreaks.join(''));
+// }
 
 // TODO: Optimize to not check all children?
 // Check Fields of first child and then check if any of the children have children
